7dfde2f3eaf68b0fd2442a303f95a2429aba98bd
saadzaveri26 <zaverifireking26@gmail.com>
2026-01-26 21:24:55 +0530

    02: authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,15 @@ import type { Metadata } from "next";
 import {  IBM_Plex_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { dark } from '@clerk/themes';
 
 const inter = Inter({
   variable: "--font-inter",
@@ -25,7 +34,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <ClerkProvider
+    appearance={{
+      theme: dark,
+    }}>
+     <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${plexMono.variable} antialiased`}
       >
@@ -34,11 +47,26 @@ export default function RootLayout({
         defaultTheme="dark"
         enableSystem
         disableTransitionOnChange
-      >
+       >
+        <header>
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton>
+            <button className="bg-rose-500 text-white p-2 rounded">
+              Sign Up 
+            </button>
+            </SignUpButton>
+
+          </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        </header>
           {children}
         </ThemeProvider>
         
       </body>
-    </html>
+     </html>
+    </ClerkProvider>
   );
 }
